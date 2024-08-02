d67db6c5ac73f2fb457ce4ca247979131995fa18
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2024-08-01 21:42:17 -0300

    Create 1075.cs

---
+++ 1075.cs
@@ -0,0 +1,22 @@
+using System; 
+class URI {
+
+    static void Main(string[] args) { 
+
+        int n;
+
+        n=int.Parse(Console.ReadLine());
+
+       for(int i = 0; i <10000; i++)
+       {
+           
+           if(i % n == 2)
+           {
+                Console.WriteLine(i);
+           }
+
+       }
+
+    }
+
+}
