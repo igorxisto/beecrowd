bf7708abbea1166566ad70f00bf963253fb1bce7
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2024-08-01 21:30:40 -0300

    Create 1060.cs

---
+++ 1060.cs
@@ -0,0 +1,24 @@
+using System; 
+class URI {
+    static void Main(string[] args) { 
+
+        int x=0;
+
+        double[] vetor=new double[6];
+
+        for(int i=0; i<vetor.Length; i++)
+        {
+           vetor[i]=double.Parse(Console.ReadLine());
+        }
+
+        for (int i=0; i<vetor.Length; i++)
+        {
+            if(vetor[i]>0){
+
+                x++;
+            }
+        }
+
+        Console.WriteLine("{0} valores positivos",x);
+    }
+}
