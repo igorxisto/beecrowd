af7519e51a7da0848bce858169be17e6b6cf9da4
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2024-08-01 21:43:33 -0300

    Create 1078.cs

---
+++ 1078.cs
@@ -0,0 +1,25 @@
+using System; 
+class URI {
+    static void Main(string[] args) { 
+
+        int i;
+
+        i=int.Parse(Console.ReadLine());
+
+        int[] dividends = {i};
+
+        int[] divisors = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        foreach (int divisor in divisors)
+        {
+        foreach (int dividend in dividends)
+        {
+
+            long longResult = Math.BigMul(dividend, divisor);
+            Console.WriteLine("{1} x {0} = {2}", dividend, divisor, longResult);
+        }
+        }
+
+    }
+
+}
