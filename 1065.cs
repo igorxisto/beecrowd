cacd3ecd6f91e9fbb6b5e609b9e1d780cdebfed1
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2024-08-01 21:32:58 -0300

    Create 1065.cs

---
+++ 1065.cs
@@ -0,0 +1,26 @@
+using System; 
+class URI {
+
+    static void Main(string[] args) { 
+
+        int x=0;
+
+        int[] vetor = new int[5];
+
+        for(int i=0; i<vetor.Length; i++)
+        {
+           vetor[i]=int.Parse(Console.ReadLine());
+        }
+
+        for(int i=0; i<vetor.Length; i++)
+        {
+            if(vetor[i] % 2 == 0){
+               x++;
+            }
+        }
+
+        Console.WriteLine("{0} valores pares", x);
+
+    }
+
+}
