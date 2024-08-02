553a243e9758aae4d8f13ffe21372352e2c27707
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2024-08-01 21:49:00 -0300

    Create 1080.cs

---
+++ 1080.cs
@@ -0,0 +1,23 @@
+using System; 
+class UR {
+    static void Main(string[] args) { 
+
+        int x, y, z;
+        x=y=z=0;
+
+        for(int i = 1; i < 101; i++)
+        {
+
+            x=int.Parse(Console.ReadLine());
+
+            if(x > y)
+            {
+                y = x;
+                z=i;
+            }
+        }
+
+        Console.WriteLine(y);
+        Console.WriteLine(z);
+    }
+}
