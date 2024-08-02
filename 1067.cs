cde6ab235c01c52d3b74b200e8a381d2c9c44222
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2024-08-01 21:34:22 -0300

    Create 1067.cs

---
+++ 1067.cs
@@ -0,0 +1,22 @@
+using System; 
+class URI {
+
+    static void Main(string[] args) { 
+
+       int x;
+
+       x=int.Parse(Console.ReadLine());
+
+       for(int i = 1; i <= x; i++)
+       {
+
+           if(i % 2 != 0)
+           {
+                Console.WriteLine(i);
+           } 
+       
+       }
+
+    }
+
+}
