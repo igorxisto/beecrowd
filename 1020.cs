68481bc5a90e837b27bd4a7ec6c1f2684e8254e5
Igor Xisto <96313477+igorxisto@users.noreply.github.com>
2024-08-01 21:04:13 -0300

    Create 1020.cs

---
+++ 1020.cs
@@ -0,0 +1,12 @@
+using System; 
+class URI {
+  static void Main(string[] args) { 
+  int notes, aux;
+  notes=int.Parse(Console.ReadLine());
+  Console.WriteLine("{0} ano(s)", notes/365);
+  aux = (notes%365);
+  Console.WriteLine("{0} mes(es)", aux/30);
+  aux = (aux%30);
+  Console.WriteLine("{0} dia(s)", aux/1);
+  }
+}
